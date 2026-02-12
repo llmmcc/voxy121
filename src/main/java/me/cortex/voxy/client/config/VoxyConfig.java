80645f73691026e19d2c935de54c302bea693c44
llmmcc <97648020+llmmcc@users.noreply.github.com>
2026-02-12 14:28:48 +0800

    Increase render distance and subdivision size

--- src/main/java/me/cortex/voxy/client/config/VoxyConfig.java
+++ src/main/java/me/cortex/voxy/client/config/VoxyConfig.java
@@ -27,9 +27,9 @@ public class VoxyConfig implements OptionStorage<VoxyConfig> {
     public boolean enabled = true;
     public boolean enableRendering = true;
     public boolean ingestEnabled = true;
-    public int sectionRenderDistance = 16;
+    public int sectionRenderDistance = 1024;
     public int serviceThreads = (int) Math.max(CpuLayout.getCoreCount()/1.5, 1);
-    public float subDivisionSize = 64;
+    public float subDivisionSize = 1024;
     public boolean useEnvironmentalFog = true;
     public boolean dontUseSodiumBuilderThreads = false;
 
